0cdac1e9f3af3948500e7c88c31dab5842358c63
gustin <gustin@entryway.net>
2017-07-03 21:38:59 -0400

    Initial commit, mix new --sup

---
+++ lib/cob_elixir.ex
@@ -0,0 +1,18 @@
+defmodule CobElixir do
+  @moduledoc """
+  Documentation for CobElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CobElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cob_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule CobElixirTest do
+  use ExUnit.Case
+  doctest CobElixir
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
