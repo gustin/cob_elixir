d919b918947a8a8a3763b8b2060c41f70a84f8c3
gustin <gustin@entryway.net>
2017-07-03 21:45:26 -0400

    Remove unneeded comments from mix generated project.

--- lib/cob_elixir/application.ex
+++ lib/cob_elixir/application.ex
@@ -1,6 +1,4 @@
 defmodule CobElixir.Application do
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,14 +6,11 @@ defmodule CobElixir.Application do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    # Define workers and child supervisors to be supervised
     children = [
       # Starts a worker by calling: CobElixir.Worker.start_link(arg1, arg2, arg3)
       # worker(CobElixir.Worker, [arg1, arg2, arg3]),
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: CobElixir.Supervisor]
     Supervisor.start_link(children, opts)
   end
