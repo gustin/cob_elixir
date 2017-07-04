bf33a44177a4c59c54b3b3bdd89c63055e100a69
gustin <gustin@entryway.net>
2017-07-03 22:06:25 -0400

    Failing test, assert if a server is listening on port 8080.

--- test/cob_elixir_test.exs
+++ test/cob_elixir_test.exs
@@ -2,7 +2,8 @@ defmodule CobElixirTest do
   use ExUnit.Case
   doctest CobElixir
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "that a server is listening" do
+    opts = [:binary, active: false]
+    assert {:ok, socket} = :gen_tcp.connect('localhost', 8080, opts)
   end
 end
