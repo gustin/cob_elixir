e4975494ae54568eb09121df877ce7b46691b1bd
gustin <gustin@entryway.net>
2017-07-17 10:52:26 -0400

    Supervisor tree doesn't need to be started in tests, apparently.

--- test/cob_elixir_test.exs
+++ test/cob_elixir_test.exs
@@ -3,15 +3,6 @@ defmodule CobElixirTest do
   doctest CobElixir
 
   test "that a server is accepting connections" do
-    import Supervisor.Spec
-
-    children = [
-      worker(Task, [CobElixir, :accept, []]),
-    ]
-
-    opts = [strategy: :one_for_one, name: CobElixir.Supervisor]
-    Supervisor.start_link(children, opts)
-
     opts = [:binary, active: true]
     assert {:ok, _socket} = :gen_tcp.connect('localhost', 8080, opts)
   end
