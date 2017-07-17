65d43f266ba5ed984f0f6c4426ff5667c371e23c
gustin <gustin@entryway.net>
2017-07-17 14:17:32 -0400

    Failing test to check for basic GET request.

--- test/cob_elixir_test.exs
+++ test/cob_elixir_test.exs
@@ -6,4 +6,11 @@ defmodule CobElixirTest do
     opts = [:binary, active: true]
     assert {:ok, _socket} = :gen_tcp.connect('localhost', 8080, opts)
   end
+
+  test "that server handles a GET request" do
+    opts = [:binary, active: true]
+    {:ok, socket } = :gen_tcp.connect('localhost', 8080, opts)
+    :gen_tcp.send(socket, "GET /hello-world HTTP/1.1")
+    assert { :status, 200 } = :gen_tcp.recv(socket, 0)
+  end
 end
