6f06d02c8684671d0c574a2d7c88fda56ee195e3
gustin <gustin@entryway.net>
2017-07-17 17:26:53 -0400

    Read and write on the socket, simply return a 200 no matter what.

--- lib/cob_elixir.ex
+++ lib/cob_elixir.ex
@@ -22,7 +22,18 @@ defmodule CobElixir do
 
   defp serve(socket) do
     socket
+    |> read_line()
+    |> write_line(socket)
 
     serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, "200")
   end
 end
