2449f4786384ccec84db42c40c1bec35a6519b61
gustin <gustin@entryway.net>
2017-07-18 12:38:25 -0400

    Refactor with Colin, loops uneeded, comment cruft.

--- lib/cob_elixir.ex
+++ lib/cob_elixir.ex
@@ -1,12 +1,6 @@
 defmodule CobElixir do
   require Logger
 
-  @moduledoc """
-  Documentation for CobElixir.
-  """
-
-  @doc """
-  """
   def accept do
     opts = [:binary, packet: :raw, active: false, reuseaddr: true]
     {:ok, socket} = :gen_tcp.listen(8080, opts)
@@ -18,28 +12,24 @@ defmodule CobElixir do
   defp loop_acceptor(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
     :ok = serve(client)
-    loop_acceptor(socket)
   end
 
-  defp serve(socket) do
-    socket
+  defp serve(client) do
+    client
     |> read_request()
-    |> write_response(socket)
+    |> write_response(client)
 
-    :gen_tcp.close(socket)
-    serve(socket)
+    :gen_tcp.close(client)
   end
 
   defp read_request(socket) do
     {:ok, data} = :gen_tcp.recv(socket, 0)
-    Logger.info "Read #{data}"
     CobElixir.Request.parse(data)
   end
 
   defp write_response({:get, {:url, url}}, socket) do
     #    status = CobElixir.Response.two_hundread_status
     status = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nHello World!"
-    Logger.info "Write #{status}"
     :gen_tcp.send(socket, status)
   end
 
