13a50ffd0cdbe7a1303f2325aeb210c4c2e8b8fe
gustin <gustin@entryway.net>
2017-07-17 23:50:01 -0400

    Parse url for GET request.

--- lib/cob_elixir/request.ex
+++ lib/cob_elixir/request.ex
@@ -1,14 +1,14 @@
 defmodule CobElixir.Request do
   @doc ~S"""
-  Parses the given `line` into a command.
+  Parses a GET request.
 
   ## Examples
 
-      iex> CobElixir.Request.parse "GET \hello-world HTTP/1.1\r\n"
+      iex> CobElixir.Request.parse "GET hello-world HTTP/1.1\r\n"
       {:get, {:url, "hello-world"}}
   """
-  def parse(request) do
-
-
+  def parse("GET" <> request) do
+    [_, url, _] = String.split(request, " ")
+    {:get, {:url, url}}
   end
 end
