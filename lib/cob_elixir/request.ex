b68d7abfbcd18bd450e6d8b54059d05ad30286ca
gustin <gustin@entryway.net>
2017-07-17 23:21:42 -0400

    Add test condition by creating an example in the doc of the actual module.

--- lib/cob_elixir/request.ex
+++ lib/cob_elixir/request.ex
@@ -1,3 +1,14 @@
 defmodule CobElixir.Request do
+  @doc ~S"""
+  Parses the given `line` into a command.
 
+  ## Examples
+
+      iex> CobElixir.Request.parse "GET \hello-world HTTP/1.1\r\n"
+      {:get, {:url, "hello-world"}}
+  """
+  def parse(request) do
+
+
+  end
 end
