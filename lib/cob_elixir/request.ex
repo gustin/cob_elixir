13bdb5b7dfe13d175de12d298a0de959b8ef9095
gustin <gustin@entryway.net>
2018-02-18 14:33:07 -0600

    Handle root/home page requests ("/"), extract out methods.

--- lib/cob_elixir/request.ex
+++ lib/cob_elixir/request.ex
@@ -3,7 +3,6 @@ defmodule CobElixir.Request do
   Parses a GET request.
 
   ## Examples
-
       iex> CobElixir.Request.parse "GET / HTTP/1.1\r\n"
       {:get, {:content_type, "text/html"}}
 
@@ -19,24 +18,47 @@ defmodule CobElixir.Request do
       iex> CobElixir.Request.parse "PATCH hello-world HTTP/1.1\r\n"
       {:error, :unsupported}
   """
-  def parse("GET" <> request) do
-    [_, url | rest] = String.split(request, " ")
-    [_, extension] = String.split(url, ".")
-    {:get, {:content_type, CobElixir.MimeType.content_type(extension)}}
+  def parse("GET" <> get_request) do
+    get_request |>
+      String.trim |>
+      identify_home_page_request |>
+      break_apart
   end
 
-  def parse("POST" <> request) do
-    [_, url | rest] = String.split(request, " ")
+  def parse("POST" <> post_request) do
+    [_, url | rest] = String.split(post_request, " ")
     {:post, {:url, url}}
   end
 
-  def parse("PUT" <> request) do
-    [_, url | rest] = String.split(request, " ")
+  def parse("PUT" <> put_request) do
+    [_, url | rest] = String.split(put_request, " ")
     {:put, {:url, url}}
   end
 
-  def parse(request) do
+  def parse(everything_else) do
     {:error, :unsupported}
+  end
+
+  defp identify_home_page_request(request) do
+    [url, _] = String.split(request, " ")
+    cond do
+      String.contains?(url, ".") ->
+        {:page, request}
+      true ->
+        {:home, request}
+    end
+  end
+
+  defp break_apart({:home, request}) do
+    get_request    = String.trim(request)
+    [url, version] = String.split(get_request, " ")
+    {:get, {:content_type, "text/html"}}
+  end
+
+  defp break_apart({:page, request}) do
+    [url, version] = String.split(request, " ")
+    [file_name, extension]      = String.split(url, ".")
+    {:get, {:content_type, CobElixir.MimeType.content_type(extension)}}
   end
 end
 
